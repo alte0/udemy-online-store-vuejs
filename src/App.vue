d229419a1287e18d76b3274ca2825b6383f67dde
m.dmitriev <makc-dmv@yandex.ru>
2018-10-03 18:47:23 +0500

    Создание навигационного меню

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <h1>Hellow Vue!</h1>
+<v-app>
+  <Navbar></Navbar>
+</v-app>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 
+export default {
+  components: {
+    Navbar
+  }
+}
 </script>
