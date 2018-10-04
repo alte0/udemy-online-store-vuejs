30b4b3add24a1fc7f3443882fb48f7d4254e1ff7
m.dmitriev <makc-dmv@yandex.ru>
2018-10-04 13:26:22 +0500

    реализация страницы регистраци

--- src/components/Auth/register.vue
+++ src/components/Auth/register.vue
@@ -1,8 +1,50 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Register</h1>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Register form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                prepend-icon="person"
+                name="Email"
+                label="Email"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+                ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+                :counter="6"
+                v-model="password"
+                :rules="passwordRules"
+                ></v-text-field>
+              <v-text-field
+                prepend-icon="repeat"
+                name="confirm'password"
+                label="Confirm Password"
+                type="password"
+                :counter="6"
+                v-model="confirmPassword"
+                :rules="confirmPasswordRules"
+                ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="onSubmit"
+              :disabled="!valid"
+              >Create Account</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,8 +52,36 @@
 
 <script>
 export default {
-  date () {
-    return {}
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Name is password',
+        v => (v && v.length >= 6) || 'Password must be equal or than 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Name is password',
+        v => v === this.password || 'Password should match'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          login: this.email,
+          password: this.password
+        }
+        console.log(user)
+      }
+    }
   }
 }
 </script>
