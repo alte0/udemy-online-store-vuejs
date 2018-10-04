e574383a358472839abf57bb1b9f461dd05f30aa
m.dmitriev <makc-dmv@yandex.ru>
2018-10-04 14:06:51 +0500

    реализация главной страницы

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,7 +21,12 @@
         @click="sideNav = !sideNav"
         class="hidden-md-and-up"
         ></v-toolbar-side-icon>
-      <v-toolbar-title>Online Store</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link
+          :to="'/'"
+          class="logo"
+          >Online Store</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn
@@ -79,3 +84,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.logo {
+  cursor: pointer;
+  color: white;
+  text-decoration: none;
+}
+.logo:hover {
+  text-decoration: underline;
+}
+</style>
