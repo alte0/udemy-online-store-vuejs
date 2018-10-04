b5d2d7fa04e8b7d1d3bdcaf16cf58bc7b777799e
m.dmitriev <makc-dmv@yandex.ru>
2018-10-04 18:17:20 +0500

    реализация страницы описание товара

--- src/components/Products/Product.vue
+++ src/components/Products/Product.vue
@@ -1,17 +1,116 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Product</h1>
-      </v-flex>
-    </v-layout>
+    <section class="product mt-3 evation-10">
+      <v-layout row wrap>
+        <v-flex xs12 lg6>
+          <img class="product_img" src='https://image.ibb.co/fZzq1o/Lenovo_Legion_Y520.jpg'>
+        </v-flex>
+        <v-flex sx12 lg6>
+          <div class="product_info">
+            <h5 class="product_title display-1 mb-3 mt-3">Lorem, ipsum dolor</h5>
+            <p class="product_category title">
+              <span class="product_title">Vendor: </span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum soluta accusamus, quis fugiat ducimus provident consectetur omnis eveniet repellat dicta repellendus reiciendis voluptate pariatur fuga doloribus ipsam est quos. Corrupti?
+            </p>
+            <p class="product_price title">
+              <span class="product_title">Price: </span>
+              $ 1000
+            </p>
+            <p class="product_color">
+              <span class="product_title">Color: </span>
+              <span
+                :title="purple"
+                :style="{backgroundColor: 'purple'}"
+                class="product_color__bg"
+                ></span>
+            </p>
+            <p class="title">
+              <span class="product_title">Material: </span>Lorem
+            </p>
+            <div class="title mb-5">
+              <p class="product_title mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat deleniti quis, fugiat esse fugit vel similique repudiandae, dolores voluptatum, impedit tenetur sunt perferendis molestias asperiores aliquid iste laudantium odio voluptates?</p>
+              <v-btn color="primary" class="heading">Edit</v-btn>
+              <v-btn color="primary" class="heading">Buy</v-btn>
+            </div>
+          </div>
+        </v-flex>
+      </v-layout>
+    </section>
   </v-container>
 </template>
 
 <script>
 export default {
-  date () {
+  data () {
     return {}
   }
 }
 </script>
+
+<style scoped>
+  .product {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    border: 1px solid #1c77ad;
+    padding: 30px;
+    margin-bottom: 100px;
+  }
+  .product_img {
+    height: 100%;
+    width: 550px;
+  }
+  .product_info {
+    margin-left: 50px;
+  }
+  .product_title {
+    font-size: 1.6rem;
+    font-weight: bold;
+    color: #1875d0;
+    margin-bottom: 0;
+  }
+  .product_price {
+    color: red;
+  }
+  .product_color__bg {
+    display: inline-block;
+    width: 50px;
+    height: 20px;
+    border: 1px solid #2b2b2b;
+    border-radius: 10px;
+  }
+  @media screen and (max-width: 1270px) {
+    .product {
+      text-align: center;
+    }
+    .product_info {
+      margin-left: 0;
+    }
+  }
+  @media screen and (max-width: 620px) {
+    .product {
+      text-align: start;
+    }
+    .product_img {
+      width: 100%;
+      height: 100%;
+    }
+    .product_title {
+      text-align: center;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .product {
+      padding: 10px;
+      text-align: start;
+      margin-bottom: 120px
+    }
+    .product_img {
+      height: 250px;
+      width: 100%;
+    }
+    .product_info {
+      margin-left: 0;
+    }
+  }
+</style>
