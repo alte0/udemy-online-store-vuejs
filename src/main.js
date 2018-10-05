773255a85adaf3c724b295c9dcf222332d69db7e
m.dmitriev <makc-dmv@yandex.ru>
2018-10-05 10:55:46 +0500

    установка и настройка firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import store from './store'
+import * as fb from 'firebase/app'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,17 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    // Initialize Firebase
+    var config = {
+      apiKey: 'AIzaSyBh32wmT7qNUx5NgqLv5FtRVReqFDChz7c',
+      authDomain: 'onlinestore-857aa.firebaseapp.com',
+      databaseURL: 'https://onlinestore-857aa.firebaseio.com',
+      projectId: 'onlinestore-857aa',
+      storageBucket: 'onlinestore-857aa.appspot.com',
+      messagingSenderId: '715395881047'
+    }
+    fb.initializeApp(config)
+  }
 })
