97a6b2632e70c760cedf4f3f2776ea9674131169
m.dmitriev <makc-dmv@yandex.ru>
2018-10-07 12:07:02 +0500

    загрузка товаров из fb

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -18,45 +18,14 @@ class Product {
 
 export default {
   state: {
-    products: [
-      {
-        id: '1',
-        title: 'Lenovo Legion Y520',
-        vendor: 'Lenovo',
-        color: 'black',
-        material: 'metal/plastic',
-        description: 'Intel Core i5 7300HQ 2500 MHz/15.6"/1920x1080/8Gb/1000Gb HDD/DVD нет/NVIDIA GeForce GTX 1050, 4096 МБ/Wi-Fi/Bluetooth/Win 10 Home',
-        price: 760,
-        promo: false,
-        imageSrc: 'https://image.ibb.co/fZzq1o/Lenovo_Legion_Y520.jpg'
-      },
-      {
-        id: '2',
-        title: 'Asus FX503VD',
-        vendor: 'Asus',
-        color: 'white',
-        material: 'plastic',
-        description: 'Intel Core i5 7300HQ 2500 MHz/15.6"/1920x1080/8Gb/256Gb SSD/DVD нет/NVIDIA GeForce GTX 1050/Wi-Fi/Bluetooth/Windows 10 Home',
-        price: 984,
-        promo: true,
-        imageSrc: 'https://image.ibb.co/cpScgo/ASUS_FX503_VD.jpg'
-      },
-      {
-        id: '3',
-        title: 'ASUS TUF Gaming FX504GD',
-        vendor: 'Asus',
-        color: 'black',
-        material: 'metal/plastic',
-        description: 'Intel Core i7 8750H 2300 MHz/15.6"/1920x1080/12Gb/1000Gb HDD/DVD нет/NVIDIA GeForce GTX 1050, 4096 МБ/Wi-Fi/Bluetooth/Win 10 Home',
-        price: 1220,
-        promo: true,
-        imageSrc: 'https://image.ibb.co/jBZOMo/ASUS_TUF_Gaming_FX504_GD.jpg'
-      }
-    ]
+    products: []
   },
   mutations: {
     createAddProduct (state, payload) {
       state.products.push(payload)
+    },
+    loadProducts (state, payload) {
+      state.products = payload
     }
   },
   actions: {
@@ -87,6 +56,37 @@ export default {
         commit('setError', error.massage)
         commit('setLoading', false)
         throw error
+      }
+    },
+    async fetchProducts ({commit}) {
+      commit('clearError')
+      commit('setLoading', true)
+      const resultProducts = []
+      try {
+        const productsVal = await fb.database().ref('products').once('value')
+        const products = productsVal.val()
+        Object.keys(products).forEach(key => {
+          const product = products[key]
+          resultProducts.push(
+            new Product(
+              product.title,
+              product.vendor,
+              product.color,
+              product.material,
+              product.price,
+              product.description,
+              product.ownerId,
+              product.imageSrc,
+              product.promo,
+              key
+            )
+          )
+          commit('loadProducts', resultProducts)
+          commit('setLoading', false)
+        })
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
       }
     }
   },
